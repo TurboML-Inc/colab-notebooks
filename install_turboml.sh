d2775871c4bb9a86afcd7fde93fafffb1b2fb007
0xn4utilus <gyanendrabanjare8@gmail.com>
2024-12-14 20:38:05 +0000

    feat: use pip

--- install_turboml.sh
+++ install_turboml.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-conda install -qq conda-forge::libstdcxx-ng anaconda::protobuf
+conda install -qq conda-forge::libstdcxx-ng anaconda::protobuf conda-forge::libtorch pytorch torchvision torchaudio cpuonly -c pytorch
 
-gdown --quiet "1UG8cmBLdYtEVJZG8Rgz454l722VAt_zE"
-
-unzip -qq turboml.zip -d turboml
-
-pip install -qq turboml/*.whl
-
-rm -rf turboml turboml.zip
+pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple turboml
 
 echo "Installation complete!"
