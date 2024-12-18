6b64834173dea12fb1e18e8cd118eb50811d3176
0xn4utilus <gyanendrabanjare8@gmail.com>
2024-12-18 17:30:35 +0000

    feat: update pip link

--- install_turboml.sh
+++ install_turboml.sh
@@ -2,6 +2,6 @@
 
 conda install -qq conda-forge::libstdcxx-ng anaconda::protobuf conda-forge::libtorch conda-forge::pytorch conda-forge::torchvision conda-forge::torchaudio
 
-pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple turboml
+pip install turboml-sdk
 
 echo "Installation complete!"
