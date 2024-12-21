defb1b54481a571649da11948ff3e88ac405ebff
0xn4utilus <gyanendrabanjare8@gmail.com>
2024-12-21 22:58:23 +0000

    feat: add setup_env script

--- install_turboml.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conda install -qq conda-forge::libstdcxx-ng anaconda::protobuf conda-forge::libtorch conda-forge::pytorch conda-forge::torchvision conda-forge::torchaudio
-
-pip install turboml-sdk
-
-echo "Installation complete!"
